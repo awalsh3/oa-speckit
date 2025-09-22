7c6ecb03121498d87f6f63bb280f2fa06af29aa0
Annie Walsh <annie@anyonething.co.uk>
2025-09-22 14:39:53 +0100

    Runs rubocop fixes

--- backend/src/models/resource.rb
+++ backend/src/models/resource.rb
@@ -2,6 +2,6 @@
 class Resource < ApplicationRecord
   belongs_to :hashtag
   validates :type, :title, :url, :description, :status, presence: true
-  validates :url, format: URI::regexp(%w[http https])
+  validates :url, format: URI.regexp(%w[http https])
   validates :status, inclusion: { in: %w[active broken] }
 end
